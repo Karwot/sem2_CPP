85e1a3fd5f4cdfd91f025f3266f44e7d3c30423b
KacperK9 <kkarwot@sigma.ug.edu.pl>
2020-05-06 19:22:37 +0200

    lab10

---
+++ cpp/lab10/zad2.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+class Date {
+    public:
+    int day;
+    int month;
+    int year;
+
+    Date() : day(0), month(0), year(0) {}
+    Date(int _day, int _month, int _year) : day(_day), month(_month), year(_year) {}
+
+    Date & operator++() { 
+         if(day==31) {
+            if(month==12) {
+                day = 1;
+                month = 1;
+                year++;
+            } else {
+            day=1;
+            month++; 
+            }
+        }
+        else 
+            day++;
+        return *this;
+    }
+    friend bool operator<(const Date& d1, const Date& d2);
+    friend ostream & operator << (ostream &out, const Date &d);
+    friend istream & operator << (istream &in, Date &d);
+
+};
+
+istream & operator << (istream &in, Date &d) {
+    cout << "dzien:\n";
+    in >> d.day;
+    cout << "miesiac:\n";
+    in >> d.month;
+    cout << "rok:\n";
+    in >> d.year;
+    return in;
+}
+
+ostream & operator << (ostream &out, const Date &d) {
+    if(d.day<10)
+    out << 0;
+    out << d.day << ".";
+    if(d.month<10)
+        out << 0; 
+    out << d.month << "." << d.year;
+    return out;
+}
+
+bool operator<(const Date& d1, const Date& d2) {
+    if(d1.year!=d2.year)
+        return d1.year<d2.year;
+    if(d1.month!=d2.month)
+        return d1.month<d2.month;
+
+    return d1.day<d2.day;
+}
+
+void cout7followigDays(Date d) {
+    for(int i=0; i<7;i++) {
+        cout << ++d << endl;
+    }
+}
+
+void compareAndCout(Date d1, Date d2) {
+    cout << "Data pozniejsza z podanych podanych: ";
+    if(d1<d2) {
+        cout << d2 << "\n7 dni po tej dacie: \n"; 
+        cout7followigDays(d2);
+    }
+    else {
+        cout << d1 << "\n7 dni po tej dacie: \n";
+        cout7followigDays(d1);
+    } 
+}
+
+int main() {
+    Date d1(31,12,2012);
+    Date d2(17,3,2010);
+    Date d3;
+    cout << "Wynik programu dla gotowych wartosci: \n";
+    cout << "\nData nr 1:" <<  d1 << " Data nr1 +1 dzien: " << ++d1 <<  "\nData nr2: " << d2 << "\nKonstruktor domyslny: " << d3 << "\n";
+    
+    compareAndCout(d1, d2);
+
+    Date u1, u2;
+
+    cout << "________________________________\n";
+    cout << "dla wartosci podanych przez uzytkownika: \n";
+    cout << "Data nr 1: \n";
+    cin << u1;
+    cin << u2;
+    compareAndCout(u1, u2);
+    return 0;
+}
---
+++ cpp/lab10/zad3.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Name {
+public:
+    string imie;
+    string nazwisko;
+
+    Name() {} 
+    Name(string _imie, string _nazwisko) : imie(_imie), nazwisko(_nazwisko) {}
+
+    string initials() {
+        string result ="";
+        result += imie[0];
+        result += nazwisko[0];
+        return result;
+    }
+
+   friend bool operator<(const Name& data1, const Name& data2);
+   friend ostream & operator << (ostream &out, const Name &n);
+   friend istream & operator >> (istream &in, Name &n);
+
+};
+
+ostream & operator << (ostream &out, const Name &n) {
+    out << n.imie + " " + n.nazwisko;
+    return out;
+};
+
+istream & operator >> (istream &in, Name &n) {
+    cout << "Imie: " << endl;
+    in >> n.imie;
+    cout << "Nazwisko: " << endl;
+    in >> n.nazwisko;
+    return in;
+}
+
+bool compareStrings(string str1, string str2) { // funkcja zwraca true jesli napis str1 poprzedza alfabetycznie napis str2 lub gdy są te same
+    int i = 0;
+    do {
+        if(str1[i]!=str2[i])
+            return str1[i]<str2[i];
+        i++;
+    } while ( str1[i]!=str2[i] && i < max( str1.length(), str2.length() )  );
+    return str1[0] < str2[0];
+};
+
+bool operator<(const Name& data1, const Name& data2) {   
+    if(data1.nazwisko==data2.nazwisko)
+        return compareStrings(data1.imie, data2.nazwisko);
+
+    return compareStrings(data1.nazwisko, data2.nazwisko);
+};
+
+int main() {
+
+    Name a1("Jan", "Kowalski");
+    Name a2("Adam", "Nowak");
+
+    cout << "Gotowe dane: \nOsoba nr 1: " << a1 << endl << "Osoba nr 2: " << a2 << endl;
+    cout << "Inicjaly osoby wystepujacej wczesniej w alfabecie: " << endl;
+    if(a1<a2)
+        cout << a1.initials();
+    else
+        cout << a2.initials();
+    cout << "\n_______________________\n Dane do wprowadzenia: " << endl;
+
+    Name n1, n2;
+    cout << "dane osoby nr1:" << endl;
+    cin >> n1;
+    cout << "dane osoby nr2:" << endl;
+    cin >> n2;
+    cout << n1.initials() << endl;
+    cout << "inicjaly osoby wypadajacej wczesniej w alfabecie: ";
+    if(n1<n2)
+        cout << n1.initials();
+    else
+        cout << n2.initials();
+    cout << endl << n2;
+
+    return 0;
+}
